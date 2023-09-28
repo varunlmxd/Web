57415495112c76d9c5a6c7e8df92911a91dd5023
Varun Shah <104298930+varunlmxd@users.noreply.github.com>
2023-09-28 22:42:16 +0530

    Add files via upload

---
+++ Rock Paper Scissor/script.js
@@ -0,0 +1,28 @@
+
+document.getElementById("0").addEventListener("click",result);
+document.getElementById("1").addEventListener("click",result);
+document.getElementById("2").addEventListener("click",result);
+
+function result(id){
+    const ai = Math.floor(Math.random()*3)
+    console.log(id.target)
+    const player =Number(id.target.id)
+    const arr = ['Rock','Paper','Scissor']
+    document.getElementById("your-answer").innerText = "YOU : "+arr[player]
+    document.getElementById("ai-answer").innerText = "AI : "+arr[ai]
+    console.log(ai,player)
+    if(ai == player)
+    document.getElementById("Result").innerText = "DRAW"
+    if(ai ==0 && player ==1)
+    document.getElementById("Result").innerText = "YOU WON!"
+    else if(ai ==0 && player ==2)
+    document.getElementById("Result").innerText = "YOU LOST!"
+    if(ai ==1 && player ==2)
+    document.getElementById("Result").innerText = "YOU WON!"
+    else if(ai ==1 && player ==0)
+    document.getElementById("Result").innerText = "YOU LOST!"
+    if(ai ==2 && player ==0)
+    document.getElementById("Result").innerText = "YOU WON!"
+    else if(ai ==2 && player ==1)
+    document.getElementById("Result").innerText = "YOU LOST!"
+}
